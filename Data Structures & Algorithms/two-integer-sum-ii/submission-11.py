05f28caa103db57c9ef5f4c03e3f36b874c9f7b8
Luciano M. <132591575+luciano665@users.noreply.github.com>
2026-09-24 08:30:04 -0400

    Add: two-integer-sum-ii - submission-11

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-11.py
@@ -0,0 +1,29 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # We will a slidding window appaorch with 2 pointer
+        # Array is sorted in increasing order
+        # So small elemts on th eleft and big on right
+        # Index1 and index 2 sum up to target 
+        # if sum is greater we decrease right pointer 
+        # If sum less increase left pointer
+
+        l = 0
+        r = len(numbers)-1
+
+        while l < r:
+
+            currSum = numbers[l] + numbers[r]
+
+            if currSum > target:
+                r -= 1
+
+            elif currSum < target:
+                l += 1
+            # Else we found the target bc sum is not lager nor smaller 
+            # Meaning must be target sum
+            else:
+                return [l+1, r+1]
+        
+        return []
+
+            
